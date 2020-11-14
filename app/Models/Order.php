ccf6e4f1e67d33e614124866aae90da7a53d0fed
Ekman <ek0519@gmail.com>
2020-11-14 19:15:18 +0800

    fill model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+
+    protected $casts = [
+        'meal_raw' => 'array'
+    ];
+
+    protected $fillable = [
+        'order_no',
+        'meal_raw',
+        'amount',
+        'status',
+        'user_id',
+        'purchase_id',
+    ];
+
+//    public function user()
+//    {
+//        $this->belongsTo('App\Models\User');
+//    }
+//
+//    public function purchase()
+//    {
+//        $this->belongsTo('App\Models\Purchase');
+//    }
 }
--- app/Models/Purchase.php
+++ app/Models/Purchase.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Purchase extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'purchase_no',
+        'user_id',
+        'amount',
+        'status',
+    ];
+
+//    public function user()
+//    {
+//        $this->belongsTo('App\Models\User');
+//    }
+//
+//    public function order()
+//    {
+//        return $this->hasOne('App\Models\Order');
+//
+//    }
 }
